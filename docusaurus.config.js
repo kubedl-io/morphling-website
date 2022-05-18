3dfbecf23bb048d478f86ff435b935b1131a3a81
dadiaokua <907837030@qq.com>
2022-05-18 14:27:19 +0800

    fix morphling github web site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/kubedl-io/morphling',
-            label: 'GitHub',
+            label: 'Morphling GitHub',
             position: 'right',
           },
         ],
